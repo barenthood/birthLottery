cd70caf802650f1dbd3a3e246e2d008b62454f41
barenthood <60615718+barenthood@users.noreply.github.com>
2020-02-20 15:23:05 -0800

    Add files via upload

---
+++ onePercent.py
@@ -0,0 +1,40 @@
+import random, sys, time
+
+worldPop = 7530000000
+billionPop = 2153
+
+oneOutOf = int(worldPop / billionPop)
+
+def birthLottery():
+            result = random.randint(0, oneOutOf)
+            print('Calculating your results...')
+            time.sleep(2)
+            print('......')
+            time.sleep(1)
+            input('Results ready, press any key to view. ')
+            if result == 3:
+                print('Congratulations! You are officially part of the 1%.')
+            else:
+                print('Sorry! You are one of the myriad unlucky ones.')
+            print('Again, y/n?')
+            again = input('> ')
+            if again == 'y':
+                birthLottery()
+            else:
+                sys.exit()
+
+
+def runLottery():
+    while True:
+        print('Run lottery simulation, y/n?')
+        choice = input('> ')
+        if choice == 'y':
+            birthLottery()
+        elif choice == 'n':
+            print('Thank you goodbye')
+            sys.exit()
+        else:
+            print('I didn\'t understand that, lets try again.')
+            return
+
+runLottery()
